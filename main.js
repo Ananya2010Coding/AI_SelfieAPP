f9f0ffcb26811c2b20d5cd8e42e656d87f3e02ee
Ananya2010Coding <85874739+Ananya2010Coding@users.noreply.github.com>
2021-07-28 21:05:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,5 +12,23 @@ recognition.onresult = function run(event){
     console.log(content);
 
     document.getElementById("voice_output").innerHTML = content;
-    
+    speak();
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("voice_output").value;
+    var utter_this = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utter_this);
+    Webcam.attach(camera);
+}
+
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
